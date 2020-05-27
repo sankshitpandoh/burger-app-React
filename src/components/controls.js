de66dadee9d37da14cf87c0866c821c4f50896db
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-27 14:52:12 +0530

    add slice and remove slice functions converted into one

--- src/components/controls.js
+++ src/components/controls.js
@@ -6,10 +6,10 @@ function Controls(props){
     return(
         <div className="controls-container">
             {/* calling single control with specific props to get specific data to display  */}
-            <SingleControl addSlice = {props.addSlice1} removeSlice = {props.removeSlice1} slice= {props.slices[0]} slicePrice = {props.slicePrices[0]}  />
-            <SingleControl addSlice = {props.addSlice2} removeSlice = {props.removeSlice2} slice= {props.slices[1]} slicePrice = {props.slicePrices[1]}  />
-            <SingleControl addSlice = {props.addSlice3} removeSlice = {props.removeSlice3} slice= {props.slices[2]} slicePrice = {props.slicePrices[2]}  />
-            <SingleControl addSlice = {props.addSlice4} removeSlice = {props.removeSlice4} slice= {props.slices[3]} slicePrice = {props.slicePrices[3]}  />
+            <SingleControl addSlice = {props.addSlice} value={1} removeSlice = {props.removeSlice} slice= {props.slices[0]} slicePrice = {props.slicePrices[0]}  />
+            <SingleControl addSlice = {props.addSlice} value={2} removeSlice = {props.removeSlice} slice= {props.slices[1]} slicePrice = {props.slicePrices[1]}  />
+            <SingleControl addSlice = {props.addSlice} value={3} removeSlice = {props.removeSlice} slice= {props.slices[2]} slicePrice = {props.slicePrices[2]}  />
+            <SingleControl addSlice = {props.addSlice} value={4} removeSlice = {props.removeSlice} slice= {props.slices[3]} slicePrice = {props.slicePrices[3]}  />
             <div className = "total-price-container">
                <h2>Total Price : </h2>
                <h2> Rs {props.totalPrice}</h2>
